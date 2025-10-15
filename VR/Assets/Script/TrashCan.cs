5aa105018c86ac8b1d848bd49beb6f53e1419aa9
mancheoldeveloper <mancheoldeveloper@gmail.com>
2025-10-15 14:02:53 +0900

    Part 3. Grab&Throw

---
+++ VR/Assets/Script/TrashCan.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class TrashCan : MonoBehaviour
+{
+
+    private GameObject leftHandModel;
+    private GameObject rightHandModel;
+
+    private void Start()
+    {
+       XRGrabInteractable grabInteractable = GetComponent<XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(HideGrabbingHand);
+        grabInteractable.selectEntered.AddListener(ShowGrabbingHand);
+    }
+
+    public void HideGrabbingHand(SelectEnterEventArgs args)
+    {
+        if(args.interactorObject.transform.tag==("Left Hand"))
+            {
+            leftHandModel.SetActive(false);
+            }
+
+       else if (args.interactorObject.transform.tag == ("Right Hand"))
+             {
+           rightHandModel.SetActive(false);
+        }
+    }
+
+    public void ShowGrabbingHand(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.tag == ("Left Hand"))
+        {
+            leftHandModel.SetActive(true);
+        }
+
+        else if (args.interactorObject.transform.tag == ("Right Hand"))
+        {
+            rightHandModel.SetActive(true);
+        }
+    }
+}
